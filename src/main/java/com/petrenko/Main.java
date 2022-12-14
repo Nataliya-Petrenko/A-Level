f5c8ce14cbd2c0364f41af29f4d38f8bb1c280a7
Nataliya <nataliya_petrenko_@ukr.net>
2022-12-14 12:35:25 +0200

    Homework 13 LJ3-262

--- src/main/java/com/petrenko/Main.java
+++ src/main/java/com/petrenko/Main.java
@@ -3,25 +3,37 @@ package com.petrenko;
 import com.petrenko.model.*;
 import com.petrenko.repository.CarRepository;
 import com.petrenko.service.CarService;
-
-import java.util.Optional;
+import com.petrenko.util.AlgorithmUtil;
 
 public class Main {
     public static void main(String[] args) {
         final CarService carService = new CarService(new CarRepository());
 
-        Car car = carService.create(Type.randomType());
-//
-//        carService.printManufacturerAndCount(car);
-//        carService.printColor(car);
-////        carService.checkCount(car);
-//        carService.printEngineInfo(car);
-//        carService.printInfo(car);
+        Car[] cars = carService.create(5);
 
+        System.out.println("Array of cars:");
+        carService.printAll();
 
-        Car car2 = carService.create();
-        System.out.println(carService.carEquals(car, car));
+        String carId = cars[3].getUuidOfCar();
+        System.out.println("Car which we'll look for: ");
+        carService.printByUuid(carId);
 
+        System.out.println("Sorted array of cars:");
+        Car[] sortedCars = AlgorithmUtil.bubbleSort(cars);
+        carService.printAll(sortedCars);
+
+        int indexOfCar = AlgorithmUtil.binarySearch(cars, carId);
+        System.out.println("Index the car (binarySearch): " + indexOfCar);
+
+        int indexOfCar1 = AlgorithmUtil.binarySearch(cars, "hhsdjhdshj");
+        System.out.println("Index the car (binarySearch): " + indexOfCar1);
+
+        int indexOfCar2 = AlgorithmUtil.recursiveBinarySearch(cars, 0, cars.length - 1, carId);
+        System.out.println("Index the car (recursiveBinarySearch): " + indexOfCar2);
+
+        int indexOfCar3 = AlgorithmUtil.recursiveBinarySearch(cars, 0, cars.length - 1,
+                "hhsdjhdshj");
+        System.out.println("Index the car (recursiveBinarySearch): " + indexOfCar3);
 
     }
 }
