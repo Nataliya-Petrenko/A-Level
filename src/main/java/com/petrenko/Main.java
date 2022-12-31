7995391f8575e43b84e8f8267e520828905f6349
Nataliya <nataliya_petrenko_@ukr.net>
2022-12-31 18:38:51 +0200

    Homework 17 LJ3-328

--- src/main/java/com/petrenko/Main.java
+++ src/main/java/com/petrenko/Main.java
@@ -1,12 +1,11 @@
 package com.petrenko;
 
-import com.petrenko.action.Actions;
-import com.petrenko.container.CarComparator;
-import com.petrenko.container.CarList;
-import com.petrenko.container.CarTree;
-import com.petrenko.container.GenericContainer;
 import com.petrenko.model.Car;
+import com.petrenko.model.Color;
+import com.petrenko.model.Type;
 import com.petrenko.service.CarService;
+
+import java.util.*;
 
 
 public class Main {
@@ -15,23 +14,59 @@ public class Main {
         CarService carService = CarService.getInstance();
 
         Car[] cars = carService.create(10);
-        for (int i = 0; i < cars.length; i++) {
-            cars[i].restore();
-        }
+
         System.out.println("All cars from CarRepository:");
         carService.printAll();
         System.out.println();
 
-        CarTree carTree = new CarTree();
+        int price = 500_000_000;
+        carService.findManufacturerByPrice(cars, price);
+        System.out.println();
 
-        for (int i = 0; i < cars.length; i++) {
-            carTree.add(cars[i]);
-        }
+        System.out.println("Number of all cars:");
+        System.out.println(carService.countSum(cars));
+        System.out.println();
+
+        System.out.println("Manufacturers and types of cars:");
+        System.out.println(carService.mapToMap(cars));
+        System.out.println();
+
+        System.out.println("Car price statistics:");
+        System.out.println(carService.statistic(cars));
+
+        price = 10;
+        System.out.println("Prices for all cars are higher than " + price + ":");
+        System.out.println(carService.priceCheck(cars, price));
+        price = 1000000000;
+        System.out.println("Prices for all cars are higher than " + price + ":");
+        System.out.println(carService.priceCheck(cars, price));
 
         System.out.println();
-        System.out.println(carService.mapManufacturerCount(cars));
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("type", Type.CAR);
+        map.put("manufacturer", "manufacturer");
+        map.put("color", Color.RED);
+        map.put("count", 123);
+        map.put("price", 1_000_000);
+
+        System.out.println("New car by Map:");
+        Car newCarByMap = carService.mapToObject(map);
+        carService.print(newCarByMap);
+
         System.out.println();
-        System.out.println(carService.mapPowerCar(cars));
+
+        List<Car> cars1 = Arrays.stream(carService.create(5)).toList();
+        List<Car> cars2 = Arrays.stream(carService.create(5)).toList();
+        List<Car> cars3 = Arrays.stream(carService.create(5)).toList();
+
+        List<List<Car>> listListCars = new LinkedList<>();
+        listListCars.add(cars1);
+        listListCars.add(cars2);
+        listListCars.add(cars3);
+
+        price = 500_000_000;
+        System.out.println(carService.innerList(listListCars, price));
 
     }
 
